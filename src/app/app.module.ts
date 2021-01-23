868627c98da3747726e9612c69be670457d02928
Alexey Pedyashev <alexey.pedyashev@scout24.ch>
2021-01-23 18:41:34 +0100

    feat(login): auth module with empty login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+// import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // The components, directives, and pipes that belong to this NgModule.
   declarations: [
     AppComponent
   ],
+  // The subset of declarations that should be visible and usable in the component templates of other NgModules.
+  // exports: [],
+  // Other modules whose exported classes are needed by component templates declared in this NgModule.
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // AuthModule
   ],
+  // Creators of services that this NgModule contributes to the global collection of services; 
+  // they become accessible in all parts of the app. (You can also specify providers at the component level.)
   providers: [],
+  // The main application view, called the root component, which hosts all other app views. 
+  // Only the root NgModule should set the bootstrap property.
   bootstrap: [AppComponent]
 })
 export class AppModule { }

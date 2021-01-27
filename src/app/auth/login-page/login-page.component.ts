2b8aba006b99eb4b614ffc01fb6b899d2c1b25c7
Alexey Pedyashev <alexey.pedyashev@scout24.ch>
2021-01-27 12:13:11 +0100

    feat(Button): support of disabled attribute for the raised button

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { NgModel, NgForm } from '@angular/forms';
 import { LoginRequest } from './login-request.model';
 import _map from 'lodash/map';
 
+type ErrorMessages = {
+  [key in string]: string
+}
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -10,7 +13,7 @@ import _map from 'lodash/map';
 })
 export class LoginPageComponent implements OnInit {
   @ViewChild('email', { static: true }) emailRef!: NgModel;
-
+  @ViewChild('password', { static: true }) passwordRef!: NgModel;
   model = new LoginRequest('', '');
 
   constructor() { }
@@ -20,14 +23,27 @@ export class LoginPageComponent implements OnInit {
 
   onSubmit() { console.log('submit', this.model, this.emailRef) }
 
-  get emailError(): string[]  {
-    const {dirty, control: {errors}} = this.emailRef;
-    let messages: string[] = []
-    const errorMessages: {[key in string]: string} = {
+  get emailErrors(): string[]  {
+    const errorMessages: ErrorMessages = {
       required: 'field is required',
       email: 'invalid email format'
     }
+    return this.getErrorMessages(this.emailRef, errorMessages);
+ 
+  }
+
+  get passwordErrors(): string[] {
+    const errorMessages: ErrorMessages = {
+      required: 'password is required',
+    }
+    return this.getErrorMessages(this.passwordRef, errorMessages);
+  }
+
+  // TODO: service?
+  getErrorMessages(elementRef: NgModel, errorMessages: ErrorMessages): string[] {
+    const {dirty, control: {errors}} = elementRef;
     
+    let messages: string[] = [];
     if (dirty && errors) {
       messages = _map(errors, (value: boolean, ruleName: string) => errorMessages[ruleName]) as unknown as string[];
     }
--- src/app/ui/button/button-raised.directive.ts
+++ src/app/ui/button/button-raised.directive.ts
@@ -1,33 +1,77 @@
 import { Directive, Input, ElementRef } from '@angular/core';
+import _values from 'lodash/values';
 import {Color, ColorsMap} from './types'
-
 @Directive({
-  selector: '[app-ui-button-raised]'
+  selector: '[app-ui-button-raised]',
 })
 export class UiButtonRaisedDirective {
   @Input() color: Color = '';
 
-
-  private defaultClasses = 'py-1 px-3 border border-gray-300 rounded text-white text-center cursor-pointer focus:outline-none hover:shadow'
+  private defaultClasses = 'py-1 px-3 border border-gray-300 rounded text-white text-center focus:outline-none hover:shadow'
+  private colorClassesMap = {
+    primaryEnabled: 'bg-blue-800 cursor-pointer',
+    primaryDisabled: 'bg-blue-300 cursor-default',
+    accentEnabled: 'bg-red-500 cursor-pointer',
+    accentDisabled: 'bg-red-200 cursor-default',
+    'default': 'text-current',
+  };
 
   constructor(private el: ElementRef) {} 
 
   ngOnInit() {
-   const { classList }  = this.el.nativeElement
+  }
+
+  // NOTE: without setter the directive reacts to the 'disabled' changes (ngOnChanges is called), 
+  // but the button will be always enabled
+  @Input('disabled')
+  set disabled(value: boolean) {
+    if(value) {
+      this.el.nativeElement.setAttribute('disabled', value);
+      this.setClasses(value);
+    } else {
+      this.el.nativeElement.removeAttribute('disabled');
+      this.setClasses(false);
+    }
+  }
+
+  setClasses(disabled: boolean) {
+    const { classList }  = this.el.nativeElement;
+    // add static classes
     this.defaultClasses.split(' ').forEach((className) => {
       classList.add(className);
     })
-    classList.add(this.colorClass());
+
+    // remove the dynamic classes before updating them
+    _values(this.colorClassesMap).forEach((currentClassList: string) => {
+      this.removeClasses(currentClassList);
+    })
+
+    // add dynamic classes
+    this.addClasses(this.colorClasses(disabled));
   }
 
-  colorClass(): string {
+  colorClasses(disabled: boolean): string {
+    const { color, colorClassesMap} = this;
     const bgColorsMap: ColorsMap = {
-      primary: 'bg-blue-800',
-      accent: 'bg-red-500',
+      primary: disabled ? colorClassesMap.primaryDisabled : colorClassesMap.primaryEnabled,
+      accent: disabled ? colorClassesMap.accentDisabled : colorClassesMap.accentEnabled,
       '': ''
     }
     
-    return bgColorsMap[this.color] || 'text-current';
+    return bgColorsMap[color] || colorClassesMap.default;
   }
 
+  addClasses(classes: string) {
+    const { classList }  = this.el.nativeElement;
+    classes.split(' ').forEach((currentClass: string) => {
+      classList.add(currentClass);
+    })
+  }
+
+  removeClasses(classes: string) {
+    const { classList }  = this.el.nativeElement;
+    classes.split(' ').forEach((currentClass: string) => {
+      classList.remove(currentClass);
+    })
+  }
 }

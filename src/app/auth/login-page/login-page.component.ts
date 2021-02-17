a046b2ee7054a8bc533c8198730cbbdd353b2e6a
Alexey Pedyashev <alexey.pedyashev@scout24.ch>
2021-02-17 19:56:09 +0100

    login validation

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -6,8 +6,8 @@ import { AuthService } from '../auth.service';
 import _map from 'lodash/map';
 
 type ErrorMessages = {
-  [key in string]: string
-}
+  [key in string]: string;
+};
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -16,44 +16,52 @@ export class LoginPageComponent implements OnInit {
   @ViewChild('email', { static: true }) emailRef!: NgModel;
   @ViewChild('password', { static: true }) passwordRef!: NgModel;
   model = new LoginRequest('', '');
-  serverResponseError: string = ''
+  serverResponseError: string = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void { 
-  }
+  ngOnInit(): void {}
 
-  onSubmit() { 
+  onSubmit() {
     this.authService.login(this.model).subscribe({
-      next() { console.log('logged in'); },
-      error: (err) => { 
-        this.serverResponseError = err
-      }
-    })
+      next() {
+        console.log('logged in');
+      },
+      error: (err) => {
+        this.serverResponseError = err;
+      },
+    });
   }
 
-  get emailErrors(): string[]  {
+  get emailErrors(): string[] {
     const errorMessages: ErrorMessages = {
-      required: 'field is required',
-      email: 'invalid email format'
-    }
+      required: 'Field is required',
+      email: 'Invalid email format',
+    };
     return this.getErrorMessages(this.emailRef, errorMessages);
   }
 
   get passwordErrors(): string[] {
     const errorMessages: ErrorMessages = {
-      required: 'password is required',
-    }
+      required: 'Password is required',
+      minlength: 'Enter at least 8 characters',
+    };
     return this.getErrorMessages(this.passwordRef, errorMessages);
   }
 
   // TODO: service?
   getErrorMessages(elementRef: NgModel, errorMessages: ErrorMessages): string[] {
-    const {dirty, control: {errors}} = elementRef;
-    
+    const {
+      dirty,
+      control: { errors },
+    } = elementRef;
+
     let messages: string[] = [];
     if (dirty && errors) {
-      messages = _map(errors, (value: boolean, ruleName: string) => errorMessages[ruleName]) as unknown as string[];
+      messages = (_map(
+        errors,
+        (value: boolean, ruleName: string) => errorMessages[ruleName] || ruleName
+      ) as unknown) as string[];
     }
     return messages;
   }

428dc558b277448e4c138d55b5410fb63a52b7ec
Alexey Pedyashev <alexey.pedyashev@scout24.ch>
2021-02-17 18:49:49 +0100

    feat(prettier): prettier and eslint with default rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule), pathMatch: 'full' }, 
-  { path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  
+  {
+    path: '',
+    loadChildren: () => import('./photos/photos.module').then((m) => m.PhotosModule),
+    pathMatch: 'full',
+  },
+  { path: '', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule) },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,16 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   // The components, directives, and pipes that belong to this NgModule.
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   // The subset of declarations that should be visible and usable in the component templates of other NgModules.
   // exports: [],
   // Other modules whose exported classes are needed by component templates declared in this NgModule.
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-    
-  ],
-  // Creators of services that this NgModule contributes to the global collection of services; 
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  // Creators of services that this NgModule contributes to the global collection of services;
   // they become accessible in all parts of the app. (You can also specify providers at the component level.)
   providers: [],
-  // The main application view, called the root component, which hosts all other app views. 
+  // The main application view, called the root component, which hosts all other app views.
   // Only the root NgModule should set the bootstrap property.
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
